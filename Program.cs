1601bcdfe96e8481a548806aef962281a5428827
Alejandro2914 <alegalindo24@hotmail.com>
2024-11-10 23:24:50 -0500

    feat: :sparkles: we changed some features of the application by adding new folders

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Mostrar el "Hola Mundo" en la consola.
-Console.WriteLine("Hola Mundo");
+// Configura el puerto para Railway (escuchar en el puerto 8080)
+app.Urls.Add("http://0.0.0.0:8080");
 
+// Muestra "Hola Mundo" en la ruta raíz
+app.MapGet("/", () => "Hola Mundo");
+
+// Inicia la aplicación
 app.Run();
